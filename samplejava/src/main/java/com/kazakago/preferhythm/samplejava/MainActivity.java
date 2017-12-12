4544215de3f3ad3043822e3b0bd9366c50b967b1
Kensuke Tamura <tamura_k@1923.co.jp>
2017-12-12 16:45:15 +0900

    improved sample module.

--- samplejava/src/main/java/com/kazakago/preferhythm/samplejava/MainActivity.java
+++ samplejava/src/main/java/com/kazakago/preferhythm/samplejava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kazakago.preferhythm.samplejava;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,115 +16,115 @@ public class MainActivity extends AppCompatActivity {
     private MyPreferencesManager myPreferencesManager = new MyPreferencesManager(this);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText valueEditText1 = findViewById(R.id.edit_value_1);
-        final TextView valueTextView1 = findViewById(R.id.text_value_1);
-        Button setButton1 = findViewById(R.id.button_set_1);
-        setButton1.setOnClickListener(new View.OnClickListener() {
+        final EditText value1EditText = findViewById(R.id.value1EditText);
+        final TextView value1TextView = findViewById(R.id.value1TextView);
+        Button set1Button = findViewById(R.id.set1Button);
+        set1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myPreferencesManager.setStringObject(valueEditText1.getText().toString());
+                myPreferencesManager.setStringObject(value1EditText.getText().toString());
                 myPreferencesManager.apply();
 
-                valueTextView1.setText(myPreferencesManager.getStringObject());
+                value1TextView.setText(myPreferencesManager.getStringObject());
             }
         });
-        Button removeButton1 = findViewById(R.id.button_remove_1);
-        removeButton1.setOnClickListener(new View.OnClickListener() {
+        Button remove1Button = findViewById(R.id.remove1Button);
+        remove1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myPreferencesManager.removeStringObject();
                 myPreferencesManager.apply();
 
-                valueTextView1.setText(myPreferencesManager.getStringObject());
+                value1TextView.setText(myPreferencesManager.getStringObject());
             }
         });
-        valueTextView1.setText(myPreferencesManager.getStringObject());
+        value1TextView.setText(myPreferencesManager.getStringObject());
 
-        final EditText valueEditText2 = findViewById(R.id.edit_value_2);
-        final TextView valueTextView2 = findViewById(R.id.text_value_2);
-        Button setButton2 = findViewById(R.id.button_set_2);
-        setButton2.setOnClickListener(new View.OnClickListener() {
+        final EditText value2EditText = findViewById(R.id.value2EditText);
+        final TextView value2TextView = findViewById(R.id.value2TextView);
+        Button set2Button = findViewById(R.id.set2Button);
+        set2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myPreferencesManager.setStringObjectNonNull(valueEditText2.getText().toString());
+                myPreferencesManager.setStringObjectNonNull(value2EditText.getText().toString());
                 myPreferencesManager.apply();
 
-                valueTextView2.setText(myPreferencesManager.getStringObjectNonNull());
+                value2TextView.setText(myPreferencesManager.getStringObjectNonNull());
             }
         });
-        Button removeButton2 = findViewById(R.id.button_remove_2);
-        removeButton2.setOnClickListener(new View.OnClickListener() {
+        Button remove2Button = findViewById(R.id.remove2Button);
+        remove2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myPreferencesManager.removeStringObjectNonNull();
                 myPreferencesManager.apply();
 
-                valueTextView2.setText(myPreferencesManager.getStringObjectNonNull());
+                value2TextView.setText(myPreferencesManager.getStringObjectNonNull());
             }
         });
-        valueTextView2.setText(myPreferencesManager.getStringObjectNonNull());
+        value2TextView.setText(myPreferencesManager.getStringObjectNonNull());
 
-        final EditText valueEditText3 = findViewById(R.id.edit_value_3);
-        final TextView valueTextView3 = findViewById(R.id.text_value_3);
-        Button setButton3 = findViewById(R.id.button_set_3);
-        setButton3.setOnClickListener(new View.OnClickListener() {
+        final EditText value3EditText = findViewById(R.id.value3EditText);
+        final TextView value3TextView = findViewById(R.id.value3TextView);
+        Button set3Button = findViewById(R.id.set3Button);
+        set3Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    myPreferencesManager.setIntObject(Integer.valueOf(valueEditText3.getText().toString()));
+                    myPreferencesManager.setIntObject(Integer.valueOf(value3EditText.getText().toString()));
                     myPreferencesManager.apply();
                 } catch (Exception e) {
                     Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 }
 
-                valueTextView3.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
+                value3TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
             }
         });
-        Button removeButton3 = findViewById(R.id.button_remove_3);
-        removeButton3.setOnClickListener(new View.OnClickListener() {
+        Button remove3Button = findViewById(R.id.remove3Button);
+        remove3Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myPreferencesManager.removeIntObject();
                 myPreferencesManager.apply();
 
-                valueTextView3.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
+                value3TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
             }
         });
-        valueTextView3.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
+        value3TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObject()));
 
-        final EditText valueEditText4 = findViewById(R.id.edit_value_4);
-        final TextView valueTextView4 = findViewById(R.id.text_value_4);
-        Button setButton4 = findViewById(R.id.button_set_4);
-        setButton4.setOnClickListener(new View.OnClickListener() {
+        final EditText value4EditText = findViewById(R.id.value4EditText);
+        final TextView value4TextView = findViewById(R.id.value4TextView);
+        Button set4Button = findViewById(R.id.set4Button);
+        set4Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    myPreferencesManager.setIntObjectNonNull(Integer.valueOf(valueEditText4.getText().toString()));
+                    myPreferencesManager.setIntObjectNonNull(Integer.valueOf(value4EditText.getText().toString()));
                     myPreferencesManager.apply();
                 } catch (Exception e) {
                     Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 }
 
-                valueTextView4.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
+                value4TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
             }
         });
-        Button removeButton4 = findViewById(R.id.button_remove_4);
-        removeButton4.setOnClickListener(new View.OnClickListener() {
+        Button remove4Button = findViewById(R.id.remove4Button);
+        remove4Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myPreferencesManager.removeIntObjectNonNull();
                 myPreferencesManager.apply();
 
-                valueTextView4.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
+                value4TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
             }
         });
-        valueTextView4.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
+        value4TextView.setText(String.format(Locale.getDefault(), "%d", myPreferencesManager.getIntObjectNonNull()));
     }
 
 }
--- samplekotlin/src/main/java/com/kazakago/preferhythm/samplekotlin/MainActivity.kt
+++ samplekotlin/src/main/java/com/kazakago/preferhythm/samplekotlin/MainActivity.kt
@@ -2,10 +2,8 @@ package com.kazakago.preferhythm.samplekotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,86 +14,70 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val valueEditText1 = findViewById<EditText>(R.id.edit_value_1)
-        val valueTextView1 = findViewById<TextView>(R.id.text_value_1)
-        val setButton1 = findViewById<Button>(R.id.button_set_1)
-        setButton1.setOnClickListener {
-            myPreferencesManager.stringObject = valueEditText1.text.toString()
+        set1Button.setOnClickListener {
+            myPreferencesManager.stringObject = value1EditText.text.toString()
             myPreferencesManager.apply()
 
-            valueTextView1.text = myPreferencesManager.stringObject
+            value1TextView.text = myPreferencesManager.stringObject
         }
-        val removeButton1 = findViewById<Button>(R.id.button_remove_1)
-        removeButton1.setOnClickListener {
+        remove1Button.setOnClickListener {
             myPreferencesManager.removeStringObject()
             myPreferencesManager.apply()
 
-            valueTextView1.text = myPreferencesManager.stringObject
+            value1TextView.text = myPreferencesManager.stringObject
         }
-        valueTextView1.text = myPreferencesManager.stringObject
+        value1TextView.text = myPreferencesManager.stringObject
 
-        val valueEditText2 = findViewById<EditText>(R.id.edit_value_2)
-        val valueTextView2 = findViewById<TextView>(R.id.text_value_2)
-        val setButton2 = findViewById<Button>(R.id.button_set_2)
-        setButton2.setOnClickListener {
-            myPreferencesManager.stringObjectNonNull = valueEditText2.text.toString()
+        set2Button.setOnClickListener {
+            myPreferencesManager.stringObjectNonNull = value2EditText.text.toString()
             myPreferencesManager.apply()
 
-            valueTextView2.text = myPreferencesManager.stringObjectNonNull
+            value2TextView.text = myPreferencesManager.stringObjectNonNull
         }
-        val removeButton2 = findViewById<Button>(R.id.button_remove_2)
-        removeButton2.setOnClickListener {
+        remove2Button.setOnClickListener {
             myPreferencesManager.removeStringObjectNonNull()
             myPreferencesManager.apply()
 
-            valueTextView2.text = myPreferencesManager.stringObjectNonNull
+            value2TextView.text = myPreferencesManager.stringObjectNonNull
         }
-        valueTextView2.text = myPreferencesManager.stringObjectNonNull
+        value2TextView.text = myPreferencesManager.stringObjectNonNull
 
-        val valueEditText3 = findViewById<EditText>(R.id.edit_value_3)
-        val valueTextView3 = findViewById<TextView>(R.id.text_value_3)
-        val setButton3 = findViewById<Button>(R.id.button_set_3)
-        setButton3.setOnClickListener {
+        set3Button.setOnClickListener {
             try {
-                myPreferencesManager.intObject = Integer.valueOf(valueEditText3.text.toString())
+                myPreferencesManager.intObject = Integer.valueOf(value3EditText.text.toString())
                 myPreferencesManager.apply()
             } catch (e: Exception) {
                 Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
                 e.printStackTrace()
             }
 
-            valueTextView3.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
+            value3TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
         }
-        val removeButton3 = findViewById<Button>(R.id.button_remove_3)
-        removeButton3.setOnClickListener {
+        remove3Button.setOnClickListener {
             myPreferencesManager.removeIntObject()
             myPreferencesManager.apply()
 
-            valueTextView3.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
+            value3TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
         }
-        valueTextView3.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
+        value3TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObject)
 
-        val valueEditText4 = findViewById<EditText>(R.id.edit_value_4)
-        val valueTextView4 = findViewById<TextView>(R.id.text_value_4)
-        val setButton4 = findViewById<Button>(R.id.button_set_4)
-        setButton4.setOnClickListener {
+        set4Button.setOnClickListener {
             try {
-                myPreferencesManager.intObjectNonNull = Integer.valueOf(valueEditText4.text.toString())
+                myPreferencesManager.intObjectNonNull = Integer.valueOf(value4EditText.text.toString())
                 myPreferencesManager.apply()
             } catch (e: Exception) {
                 Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
                 e.printStackTrace()
             }
 
-            valueTextView4.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
+            value4TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
         }
-        val removeButton4 = findViewById<Button>(R.id.button_remove_4)
-        removeButton4.setOnClickListener {
+        remove4Button.setOnClickListener {
             myPreferencesManager.removeIntObjectNonNull()
             myPreferencesManager.apply()
 
-            valueTextView4.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
+            value4TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
         }
-        valueTextView4.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
+        value4TextView.text = String.format(Locale.getDefault(), "%d", myPreferencesManager.intObjectNonNull)
     }
 }
